d6699da8606dd4650f50618568e07fb986f41234
wathika <71040609+wathika-eng@users.noreply.github.com>
2023-01-08 00:05:51 +0300

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,15 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+
+
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
